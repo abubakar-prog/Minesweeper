15fc75d6a825ce9a1e5b171d1f95ad38547615c1
Abu Bakar Ahmad Cheema <cheemaabubakr2@gmail.com>
2025-12-09 12:20:03 +0500

    Refactor game_state.h to include additional variables

--- game_state.h
+++ game_state.h
@@ -1,8 +1,12 @@
 #ifndef GAME_STATE_H
 #define GAME_STATE_H
-
+#include <SFML/System.hpp>
 #include "constants.h"
 
+extern bool isFirstClick;
+extern bool enteringName;
+extern char playerName[MAX_NAME_LENGTH];
+extern int nameLength;
 extern int currentScreen;
 extern int grid[MAX_GRID_SIZE][MAX_GRID_SIZE];
 extern bool revealed[MAX_GRID_SIZE][MAX_GRID_SIZE];
@@ -14,11 +18,20 @@ extern int currentMineCount;
 extern int currentDifficulty;
 extern int playerScore;
 extern int cellsRevealed;
+extern int timerSeconds;
+extern bool timerStarted;
+extern sf::Clock gameClock;
+extern bool firstClick;
 extern int flagsPlaced;
 extern bool winSoundPlayed;
 extern bool loseSoundPlayed;
+extern float scaleX;
+extern float scaleY;
+extern float offsetX;
+extern float offsetY;
 extern int highScores[MAX_HIGH_SCORES];
 extern char highScoreNames[MAX_HIGH_SCORES][MAX_NAME_LENGTH];
+extern int highScoreTimes[MAX_HIGH_SCORES];
 extern int highScoreCount;
 extern bool explosion;
 void resetGameState();
