15b7e5198a5f536b3f2084e012cabc39b35188b1
Abu Bakar Ahmad Cheema <cheemaabubakr2@gmail.com>
2025-12-09 12:16:38 +0500

    Update constants.h

--- constants.h
+++ constants.h
@@ -5,11 +5,13 @@ const char MINE_IMAGE_PATH[] = "assets/minesweeper_mine.png";
 const char FLAG_IMAGE_PATH[] = "assets/minesweeper_flag.png";
 const char WIN_SOUND_PATH[] = "assets/win.wav";
 const char LOSE_SOUND_PATH[] = "assets/lose.wav";
+//const char LOSE_SOUND_PATH[] = "assets/laugh.wav";
+//const char LOSE_SOUND_PATH[] = "assets/aag.wav";
 
 const int MAX_GRID_SIZE = 10;
 const int CELL_SIZE = 50;
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 700;
+const int WINDOW_WIDTH = 1100;
+const int WINDOW_HEIGHT = 1000;
 const int GAME_AREA_OFFSET_Y = 100;
 const int MAX_HIGH_SCORES = 10;
 const int MAX_NAME_LENGTH = 20;
