5c9f40f289f5d2f770e34356c2aeaa7e302c04d3
Abu Bakar Ahmad Cheema <cheemaabubakr2@gmail.com>
2025-12-09 12:17:06 +0500

    Refactor saveGame and saveHighScores functions

--- file_handler.cpp
+++ file_handler.cpp
@@ -3,34 +3,37 @@
 #include "constants.h"
 #include <fstream>
 
+using namespace std;
+using namespace sf;
+
 void saveGame() {
     std::ofstream file("savegame.txt");
     if (!file.is_open()) return;
-    file << currentGridSize << "\n";
-    file << currentMineCount << "\n";
-    file << currentDifficulty << "\n";
-    file << cellsRevealed << "\n";
-    file << flagsPlaced << "\n";
-    file << playerScore << "\n";
-    file << gameOver << "\n";
-    file << gameWon << "\n";
+    file << currentGridSize << endl;
+    file << currentMineCount << endl;
+    file << currentDifficulty << endl;
+    file << cellsRevealed << endl;
+    file << flagsPlaced << endl;
+    file << playerScore << endl;
+    file << gameOver << endl;
+    file << gameWon << endl;
     for (int i = 0; i < MAX_GRID_SIZE; i++) {
         for (int j = 0; j < MAX_GRID_SIZE; j++) {
             file << grid[i][j] << " ";
         }
-        file << "\n";
+        file << endl;
     }
     for (int i = 0; i < MAX_GRID_SIZE; i++) {
         for (int j = 0; j < MAX_GRID_SIZE; j++) {
             file << revealed[i][j] << " ";
         }
-        file << "\n";
+        file << endl;
     }
     for (int i = 0; i < MAX_GRID_SIZE; i++) {
         for (int j = 0; j < MAX_GRID_SIZE; j++) {
             file << flagged[i][j] << " ";
         }
-        file << "\n";
+        file << endl;
     }
     file.close();
 }
@@ -64,15 +67,16 @@ void loadGame() {
     file.close();
 }
 
-void saveHighScores() {
+void saveHighScores() 
+{
     std::ofstream file("highscores.txt");
     if (!file.is_open()) return;
     file << highScoreCount << "\n";
     for (int i = 0; i < highScoreCount; i++) {
         file << highScores[i] << "\n";
         file << highScoreNames[i] << "\n";
+        file << highScoreTimes[i] << "\n";
     }
-    file.close();
 }
 
 void loadHighScores() {
@@ -83,15 +87,25 @@ void loadHighScores() {
     }
     file >> highScoreCount;
     file.ignore();
+
     for (int i = 0; i < highScoreCount; i++) {
         file >> highScores[i];
         file.ignore();
         file.getline(highScoreNames[i], MAX_NAME_LENGTH);
+
+        if (file >> highScoreTimes[i]) {
+            file.ignore();
+        }
+        else {
+            highScoreTimes[i] = 0;
+            file.clear();
+        }
     }
     file.close();
 }
 
-void addHighScore(int score, char name[]) {
+void addHighScore(int score, char name[], int time) 
+{
     int position = -1;
     for (int i = 0; i < highScoreCount; i++) {
         if (score > highScores[i]) {
@@ -105,6 +119,7 @@ void addHighScore(int score, char name[]) {
     if (position == -1) {
         position = highScoreCount;
         highScores[position] = score;
+        highScoreTimes[position] = time;
         int j = 0;
         while (j < MAX_NAME_LENGTH - 1 && name[j] != '\0') {
             highScoreNames[position][j] = name[j];
@@ -118,12 +133,14 @@ void addHighScore(int score, char name[]) {
     for (int i = MAX_HIGH_SCORES - 1; i > position; i--) {
         if (i - 1 < highScoreCount) {
             highScores[i] = highScores[i - 1];
+            highScoreTimes[i] = highScoreTimes[i - 1];
             for (int j = 0; j < MAX_NAME_LENGTH; j++) {
                 highScoreNames[i][j] = highScoreNames[i - 1][j];
             }
         }
     }
     highScores[position] = score;
+    highScoreTimes[position] = time;
     int j = 0;
     while (j < MAX_NAME_LENGTH - 1 && name[j] != '\0') {
         highScoreNames[position][j] = name[j];
@@ -139,9 +156,9 @@ void addHighScore(int score, char name[]) {
 void saveSettings() {
     std::ofstream file("settings.txt");
     if (!file.is_open()) return;
-    file << currentDifficulty << "\n";
-    file << currentGridSize << "\n";
-    file << currentMineCount << "\n";
+    file << currentDifficulty << endl;
+    file << currentGridSize << endl;
+    file << currentMineCount << endl;
     file.close();
 }
 
